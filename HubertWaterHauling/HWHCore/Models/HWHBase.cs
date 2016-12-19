4f892531e5fbae4adc33985dcc0ee5b9c9a6bfb9
Derek Scheller <derekscheller23@gmail.com>
2016-12-18 20:49:13 -0600

    program

---
+++ HubertWaterHauling/HWHCore/Models/HWHBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWHCore.Models
+{
+    public class HWHBase
+    {
+        public int Id { get; set; }
+
+    }
+}
---
+++ HubertWaterHauling/HWHCore/Models/WaterTruck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWHCore.Models
+{
+    public class WaterTruck : HWHBase
+    {
+        public string Name { get; set; }
+        public DateTime ManufactureYear { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public string Model { get; set; }
+
+    }
+}
